e1372c26ec031f5e136d48242b42c69dfb027f89
Md. Ashraful Islam <mdashrafulislam.inbox@gmail.com>
2024-09-25 22:51:14 +0600

    added user coin list fetch api

--- models/coinModel.js
+++ models/coinModel.js
@@ -6,12 +6,12 @@ const getCoinsByUserId = async (userId) => {
     SELECT 
       SUM(CASE WHEN coin_type = 'PRIMARY' THEN coin_value ELSE 0 END) AS primary_coin,
       SUM(CASE WHEN coin_type = 'SECONDARY' THEN coin_value ELSE 0 END) AS secondary_coin
-    FROM Coin WHERE uid = ?
+    FROM Coin WHERE uid = ?;
   `;
   const [result] = await db.execute(query, [userId]);
   return result.length > 0 ? result[0] : { primary_coin: 0, secondary_coin: 0 };
 };
 
 module.exports = {
-  getCoinsByUserId
+  getCoinsByUserId,
 };
